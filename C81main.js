cf70211a0b8a13332e160a05eed2a073352e4964
thebestcoder50 <77522578+thebestcoder50@users.noreply.github.com>
2022-09-27 17:09:47 -0700

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,11 @@
+ctx.beginPath();
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 4;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(220, 200, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
